cd243f47b472dc355c80a40ad3f1f86b7832b936
SAHIL KUMAR <154916552+Sahil9205@users.noreply.github.com>
2025-04-06 23:28:38 +0530

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,7 +1,11 @@
- module.exports = {
-    content: ["./src/**/*.{js,jsx,ts,tsx}"],
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      "./src/**/*.{js,jsx,ts,tsx}",
+    ],
     theme: {
-        extend: {},
+      extend: {},
     },
     plugins: [],
- };
+  }
+  
